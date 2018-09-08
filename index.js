094cfd2601462929391faf710c88e5efca74a37e
Monica  Karasek <mlkarasek@gmail.com>
2018-09-08 22:49:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ const fareDoubler = createFareMultiplier(2);
 
 const fareTripler = createFareMultiplier(3);
 
-const selectDifferentDrivers = function(drivers, func){
-  return func(drivers)
+const selectDifferentDrivers = function(drivers, function){
+  return function(drivers)
 }
